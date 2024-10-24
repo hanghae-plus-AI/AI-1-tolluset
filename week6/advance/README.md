863ea6baba9e5ea4b066fbf01b49699168e26d9f
tolluset <dlqud19@gmail.com>
2024-10-25 03:57:09 +0900

    feat: update format

--- week6/advance/README.md
+++ week6/advance/README.md
@@ -9,23 +9,23 @@
 
 ## wandb
 
-train/loss: https://api.wandb.ai/links/tolluset-ai/6bo8w7cm
+train/loss: https://api.wandb.ai/links/tolluset-ai/u4p8kyrt
 
-eval/loss: https://api.wandb.ai/links/tolluset-ai/vrrmi8dc
+eval/loss: https://api.wandb.ai/links/tolluset-ai/h1nyx8ec
 
 ## 모델 확인
 
 학습 전:
 
 ```
-비트코인 가격이 급등하며 시장의 주목을 받고 있습니다. 전문가들은 어떤 의견을 내놓고 있을까요?
-I think it's a good idea to have the same name for both of them, but I don't know if that would be allowed in the rules... :/ (Also, I'm not sure what you mean by "the same" as well) :) But yeah! It'd probably work out fine since they're two different people and all their names are similar enough already ;) Thanks anyway!! <3~<33>^(~~♡♥ㅎω∇))) ♐)))))) xD ) )) ^_X;;;:DD Dxd XD ;-.;:(^^**) ;; *(*☆★✨▶◕・めけ⏾❗💙☺️)* ◃•̯■‿●+¦¯﷍·´╮`┻━───** ▫≧°º °ˢ͟ʭƚ陂�悪ノ
+### Question: 비트코인 가격이 급등하며 시장의 주목을 받고 있습니다. 전문가들은 어떤 의견을 내놓고 있을까요?
+I think it's a good question, but I don't know the answer to that one.
 ```
 
 학습 후:
 
 ```
-비트코인 가격이 급등하며 시장의 주목을 받고 있습니다. 전문가들은 어떤 의견을 내놓고 있을까요? 에동안 사용이 상승의 지궭은 증가하는 높아지고, 이유로 커집하는 데이업성을 제공하는 물이에서 전문가들이 가능할 수
+### Question: 비트코인 가격이 급등하며 시장의 주목을 받고 있습니다. 전문가들은 어떤 의견을 내놓고 있을까요? 상승의 증가하면 에게 지속되며, 안접하면 사용이 있습니다. 규제와 가격 상승의 충동성이 있습니다. 가격 상승의 커
 ```
 
-한국어가 이상하지만 형태는 하고 있는걸 확인 가능
+한국어를 어느 정도 이해하고 답변을 만들려는 것을 확인할 수 있습니다. 하지만, 완벽한 대답을 하지는 못합니다.
--- week6/advance/train.py
+++ week6/advance/train.py
@@ -1,20 +1,26 @@
 from datasets import load_dataset
-from trl import SFTConfig, SFTTrainer
-from transformers import pipeline, TrainingArguments
+from trl import SFTConfig, SFTTrainer, DataCollatorForCompletionOnlyLM
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 model_name = "facebook/opt-350m"
-generator = pipeline("text-generation", model=model_name, device="cuda")
 
-input_text = "비트코인 가격이 급등하며 시장의 주목을 받고 있습니다. 전문가들은 어떤 의견을 내놓고 있을까요?"
-output = generator(
-    input_text,
-    max_length=250,
-    num_return_sequences=1,
-    truncation=True,
-    repetition_penalty=1.2,
-)
+def textGen(model_path):
+    generator = pipeline("text-generation", model=model_path, device="cuda")
 
-print(output[0]["generated_text"])
+    input_text = "### Question: 비트코인 가격이 급등하며 시장의 주목을 받고 있습니다. 전문가들은 어떤 의견을 내놓고 있
+을까요?"
+    output = generator(
+            input_text,
+            max_length=250,
+            num_return_sequences=1,
+            truncation=True,
+            repetition_penalty=1.2,
+    )
+
+    print(output[0]["generated_text"])
+
+# test before train
+textGen(model_name)
 
 dataset = load_dataset(
     "json",
@@ -22,18 +28,24 @@ dataset = load_dataset(
     split="train",
 )
 
+model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-def map_to_texts(examples):
-    return {"text": examples["prompt"] + " " + examples["completion"]}
+def formatting_prompts_func(example):
+    output_texts = []
+    for i in range(len(example['prompt'])):
+        text = f"### Question: {example['prompt'][i]}\n### Answer: {example['completion'][i]}"
+        output_texts.append(text)
+    return output_texts
 
-
-dataset = dataset.map(map_to_texts)
+response_template = "### Answer:"
+collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
 
 train_valid_split = dataset.train_test_split(test_size=0.2)
 train_dataset = train_valid_split["train"]
 validation_dataset = train_valid_split["test"]
 
-training_args = TrainingArguments(
+args = SFTConfig(
     output_dir="/tmp",
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
@@ -47,21 +59,14 @@ trainer = SFTTrainer(
     model_name,
     train_dataset=train_dataset,
     eval_dataset=validation_dataset,
-    args=training_args,
-    dataset_text_field="text",
+    args=args,
+    formatting_func=formatting_prompts_func,
+    data_collator=collator,
 )
 trainer.train()
-trainer.evaluate()
 
 
 model_path = "/tmp/checkpoint-30"
-generator = pipeline("text-generation", model=model_path, device="cuda")
-output = generator(
-    input_text,
-    max_length=250,
-    num_return_sequences=1,
-    truncation=True,
-    repetition_penalty=1.2,
-)
 
-print(output[0]["generated_text"])
+# test after train
+textGen(model_path)
